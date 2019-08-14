1e2150109a8ef5a26ddc14c3dbd0d1a5d142f830
Eric Esquibel <eric@abmp.com>
2019-08-14 16:13:12 -0600

    Add project files.

---
+++ App_Start/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(DeepDive2019.eWeb.API.Startup))]
+
+namespace DeepDive2019.eWeb.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(Register);
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+            RegisterBundles(BundleTable.Bundles);
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable CORS
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // MVC routes
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+        }
+    }
+}
