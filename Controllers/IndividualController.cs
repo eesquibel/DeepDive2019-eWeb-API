75fb5e434539165037ccbb834ac605368a6a351f
Eric Esquibel <eric@abmp.com>
2019-08-16 16:20:00 -0600

    Helper functions and working controller

--- Controllers/IndividualController.cs
+++ Controllers/IndividualController.cs
@@ -1,39 +1,135 @@
-﻿using System;
+﻿using Avectra.netForum.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 
 namespace DeepDive2019.eWeb.API.Controllers
 {
     public class IndividualController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        Guid? CustomerKey;
+        FacadeClass Individual;
+
+        public IndividualController() : base()
         {
-            return new string[] { "value1", "value2" };
+            CustomerKey = this.GetCustomerKey();
+            Individual = this.GetIndividual();
+        }
+
+        // GET api/<controller>
+        public IDictionary<string, object> Get()
+        {
+            return Get("ind");
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public IDictionary<string, object> Get(string id)
         {
-            return "value";
+            if (CustomerKey == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+
+            if (Individual == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            var result = new Dictionary<string, object>();
+
+            foreach (DictionaryEntry pair in Individual.GetDataObject(id).GetFields())
+            {
+                Field field = (Field)pair.Value;
+
+                if (field == null || field.ValueNative == null)
+                {
+                    continue;
+                }
+
+                // Skip hidden fields
+                if (!field.CanSelect || field.Properties.Hidden)
+                {
+                    continue;
+                }
+
+                result.Add((string)pair.Key, field.ValueNative);
+            }
+
+            return result;
         }
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
+        // PUT api/<controller>/<id>
+        public IDictionary<string, object> Put(string id, [FromBody]JObject update)
         {
-        }
+            if (CustomerKey == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            if (Individual == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            foreach (var pair in update)
+            {
+                var token = pair.Value;
+                var field = Individual.GetField(pair.Key);
+
+                // Make sure the field exists
+                if (field == null)
+                {
+                    continue;
+                }
+
+                // Skip read-only fields
+                if (!field.CanUpdate || field.Properties.ReadOnly || field.Properties.ReadOnlyEdit || field.Properties.NotEditable)
+                {
+                    continue;
+                }
+
+                // Make sure its a scalar type
+                if (token is JValue jvalue)
+                {
+                    field.ValueNative = jvalue.Value;
+                }
+            }
+
+            using (var connection = DataUtils.GetConnection())
+            {
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var error = Individual.Update(connection, transaction);
+
+                    if (error.HasError)
+                    {
+                        transaction.Rollback();
+
+                        throw new HttpResponseException
+                        (
+                            new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                            {
+                                Content = new StringContent(JsonConvert.SerializeObject(new
+                                {
+                                    error.Message,
+                                    error.UserMessage,
+                                    error.Number
+                                }), Encoding.UTF8, "application/json")
+                            }
+                        );
+                    }
+
+                    transaction.Commit();
+                }
+            }
+
+            return Get(id);
         }
     }
 }
---
+++ Extensions.cs
@@ -0,0 +1,44 @@
+﻿using Avectra.netForum.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DeepDive2019.eWeb.API
+{
+    public static class Extensions
+    {
+        public static Guid? GetCustomerKey(this ApiController apiController)
+        {
+            var Session = HttpContext.Current?.Session;
+
+            if (Session == null)
+            {
+                return null;
+            }
+
+            string customerKey = (string)Session["CustomerKey"];
+
+            if (Guid.TryParse(customerKey, out Guid result))
+            {
+                return result;
+            } else
+            {
+                return null;
+            }
+        }
+
+        public static FacadeClass GetIndividual(this ApiController apiController)
+        {
+            var Session = HttpContext.Current?.Session;
+
+            if (Session == null)
+            {
+                return null;
+            }
+
+            return (FacadeClass)Session["Individual"];
+        }
+    }
+}
